f2b0ab3667bd817fb827724328f50a032d2532f0
Vlad Postolachi <postolachi.vlad@gmail.com>
2021-02-12 03:29:56 +0100

    Add Create route, Add Destroy route, Add Create action in controller, Add Destroy action in controller, Create Show view file

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::MessagesController < Api::V1::BaseController
-  before_action :set_message, only: [:show, :update]
+  before_action :set_message, only: [:show, :update, :destroy]
 
   def index
     @messages = Message.all
@@ -10,6 +10,18 @@ class Api::V1::MessagesController < Api::V1::BaseController
 
   def update
     @message.update(message_params) ? (render :show) : render_error
+  end
+
+  def create
+    # Check if Message already exist
+    @message = Message.find_by(content: params[:message][:content])
+    # If exist increment It's counter in DB else create new Message
+    @message ? counter_increment(@message) : create_new_message
+  end
+
+  def destroy
+    @message.destroy
+    head :no_content
   end
 
   private
@@ -25,5 +37,16 @@ class Api::V1::MessagesController < Api::V1::BaseController
   def render_error
     render json: { errors: @message.errors.full_messages },
            status: :unprocessable_entity
+  end
+
+  def create_new_message
+    @message = Message.new(message_params)
+    @message.save ? (render :show, status: :created) : render_error
+  end
+
+  def counter_increment(message)
+    message.counter += 1
+    message.save
+    render :show, status: :created
   end
 end
